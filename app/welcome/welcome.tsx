4ddea2078e0c12af8f131d8c5e07a13238b751b1
Random_Guy <khoile5399@gmail.com>
2025-06-15 14:05:36 +0700

    feat: :memo: Setting up

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -30,3 +30,20 @@ export function Welcome() {
     </main>
   );
 }
+/** As per the test's constraints, I'll assume that there are 3 things that you're looking for from this OA:
+ *  1. Technical problem-solving skills relevant to the role.
+ *  2. ORIGINAL work that demonstrates GENUINE understanding.
+ *  3. You aren't expecting perfection, you just want to see me TRY my best.
+ *
+ * With that in mind, I'm going to treat this test the same way I approach ANY problem in my day-to-day job,
+ * to show you how I would ACTUALLY perform IF I have a chance to work as part of your team.
+ * Here's how it's gonna be, step-by-step:
+ *  1. I'll try my best to solve the problem WITHOUT any help.
+ *  2. I'll use external support (AI to be specific) to see if there's a better way to do it or to clean up my code.
+ *    If you're still reading this, it means that I'm already at this step ;)
+ *  3. My final submission could be either enhanced by AI or remain the same as my initial solution
+ *    (if it's good enough, which is rarely the case btw).
+ *
+ * You might think I'm cheating (guilty as charged), while I see it as being transparent about my actual workflow.
+ * Whether this approach aligns with your team's values is ultimately your call.
+ */
